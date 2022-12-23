487a37365108e56c37146d24a3533f445ff6901c
Mr. Sableye <mister.sableye@gmail.com>
2022-12-23 10:02:42 -0600

    Adds execution id for better log readability

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,37 @@
 import { performance } from 'perf_hooks';
 
+const VALID_EXECUTION_ID_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+const createExecutionId = (length = 6) => {
+  let result = '';
+
+  for ( var i = 0; i < length; i++ ) {
+    const characterIndex = Math.floor(Math.random() * VALID_EXECUTION_ID_CHARACTERS.length);
+    result += VALID_EXECUTION_ID_CHARACTERS.charAt(characterIndex);
+  }
+
+  return result;
+};
+
 export const log = (prefix: string, message: string) => {
-    console.log(`[${prefix}] ${message}`);
+  console.log(`[${prefix}] ${message}`);
 };
 
 export const logExecution = async <T>(
-    prefix: string,
-    startLabel: string,
-    endLabel: string,
-    fn: () => Promise<T>
+  prefix: string,
+  startLabel: string,
+  endLabel: string,
+  fn: () => Promise<T>
 ): Promise<T> => {
-    log(prefix, startLabel);
+  const executionId = createExecutionId();
 
-    const start = performance.now();
-    const result = await fn();
-    const end = performance.now();
+  log(prefix, `[${executionId}] ${startLabel}`);
 
-    log(prefix, `${endLabel}: ${end - start}ms`);
+  const start = performance.now();
+  const result = await fn();
+  const end = performance.now();
 
-    return result;
+  log(prefix, `[${executionId}] ${endLabel}: ${end - start}ms`);
+
+  return result;
 };
